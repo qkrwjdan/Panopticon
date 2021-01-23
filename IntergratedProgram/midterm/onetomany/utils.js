847cfdd1f499995ff2237f8d2937c72015eeff43
icankok <eyecandy2514@naver.com>
2021-01-24 01:46:56 +0900

    0124

--- IntergratedProgram/midterm/onetomany/utils.js
+++ IntergratedProgram/midterm/onetomany/utils.js
@@ -4,3 +4,6 @@ let totalScore = 0;
 function score_clear() {
     totalScore = 0;
 }
+navigator.geolocation.getCurrentPosition(function(position) {
+    document.getElementById('Loc').innerHTML = "위도:" + position.coords.latitude + ", 경도:" + position.coords.longitude;
+});
