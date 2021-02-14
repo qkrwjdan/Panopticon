fde4cca2955162cc51c99659f6f8f38e467314bd
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-02-14 20:18:47 +0900

    2/14
    2/14

--- IntergratedProgram/midterm/onetomany/broadcast-ui.js
+++ IntergratedProgram/midterm/onetomany/broadcast-ui.js
@@ -675,7 +675,7 @@ var config = {
                 dataType: "json",
                 success: function(data) {
                     console.log("IpLIST: ", data);
-                    var ipalert = "<div class='name " + index + "' style='color:black; font-size:20px; text-align: left; padding-left: 10px'>아이피도착</div>"
+                    var ipalert = "<div class='name " + index + "' style='color:black; font-size:20px; text-align: left; padding-left: 13px '>아이피도착</div>"
                     $(".parti_area").append(ipalert);
                 },
                 error: function(e) {
